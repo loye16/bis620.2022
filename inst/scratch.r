774789a1d13a5fc5c197877ddebace604d197976
Elisa Loy <37096340+loye16@users.noreply.github.com>
2022-11-21 23:29:05 -0500

    fix accel_plot

--- inst/scratch.r
+++ inst/scratch.r
@@ -10,7 +10,7 @@ data(ukb_accel)
 
 dt = first(ukb_accel$time)
 
-ukb_accel |>
+ukb_accel[1:100,] |>
   filter(time >= dt  & time < dt + minutes(5)) |>
 #  (\(x) x[seq(1, nrow(x), by =100),])() |>
   accel_plot() +
@@ -18,9 +18,10 @@ ukb_accel |>
     xlab("Time") +
     ylab("Acceleration (in gravities)")
 
-ukb_accel |>
+ukb_accel[1:100,] |>
   filter(time >= dt & time < dt + minutes(5) ) |>
-  spectral_signature(take_log = FALSE) |>
+  spectral_signature(take_log = TRUE) |>
  # filter(freq <= 10) |>
   accel_plot()
+
 
