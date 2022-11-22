d2d674cdcb9e106e2c8be67c24a1ff3ce1515eb7
Elisa Loy <37096340+loye16@users.noreply.github.com>
2022-11-22 13:31:12 -0500

    lintr edits

--- tests/testthat/test-plot.R
+++ tests/testthat/test-plot.R
@@ -2,7 +2,7 @@ test_that(
   "The accel_plot() returns a ggplot object for 'time' data",
   {
     data(ukb_accel)
-    p = accel_plot(ukb_accel[1:100,])
+    p <- accel_plot(ukb_accel[1:100, ])
     expect_true(inherits(p, "gg"))
   }
 )
@@ -11,7 +11,7 @@ test_that(
   "The accel_plot() returns a ggplot object for 'freq' data and spectral",
   {
     data(ukb_accel)
-    p =  accel_plot(ukb_accel[1:100,] |> spectral_signature(take_log = TRUE))
+    p <- accel_plot(ukb_accel[1:100, ] |> spectral_signature(take_log = TRUE))
     expect_true(inherits(p, "gg"))
   }
 )
@@ -30,7 +30,7 @@ test_that(
   "The accel_plot() is correct for time-series data.",
   {
     data(ukb_accel)
-    p = accel_plot(ukb_accel[1:100,])
+    p <- accel_plot(ukb_accel[1:100, ])
     vdiffr::expect_doppelganger("first-100-samples", p)
   }
 )
