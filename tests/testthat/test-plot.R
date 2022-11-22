570f626f826a9c16026156c383e89369a79d7f36
Elisa Loy <37096340+loye16@users.noreply.github.com>
2022-11-22 00:36:49 -0500

    add tests and remove hello.R

--- tests/testthat/test-plot.R
+++ tests/testthat/test-plot.R
@@ -1,11 +1,21 @@
 test_that(
-  "The accel_plot() returns a ggplot object.",
+  "The accel_plot() returns a ggplot object for 'time' data",
   {
     data(ukb_accel)
-    p =  accel_plot(ukb_accel[1:100,])
+    p = accel_plot(ukb_accel[1:100,])
     expect_true(inherits(p, "gg"))
   }
 )
+
+test_that(
+  "The accel_plot() returns a ggplot object for 'freq' data and spectral",
+  {
+    data(ukb_accel)
+    p =  accel_plot(ukb_accel[1:100,] |> spectral_signature(take_log = TRUE))
+    expect_true(inherits(p, "gg"))
+  }
+)
+
 
 test_that(
   "The accel_plot() errors when no time or freq column.",
@@ -14,6 +24,7 @@ test_that(
     expect_error(accel_plot(iris))
   }
 )
+
 
 test_that(
   "The accel_plot() is correct for time-series data.",
