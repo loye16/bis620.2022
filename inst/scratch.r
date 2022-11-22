326c90d545a26dc28a088981fa554af38c28977d
Elisa Loy <37096340+loye16@users.noreply.github.com>
2022-11-22 17:24:32 -0500

    more lintr

--- inst/scratch.r
+++ inst/scratch.r
@@ -8,9 +8,9 @@ document()
 
 data(ukb_accel)
 
-dt = first(ukb_accel$time)
+dt <- first(ukb_accel$time)
 
-ukb_accel[1:100,] |>
+ukb_accel[1:100, ] |>
   filter(time >= dt  & time < dt + minutes(5)) |>
 #  (\(x) x[seq(1, nrow(x), by =100),])() |>
   accel_plot() +
@@ -18,10 +18,7 @@ ukb_accel[1:100,] |>
     xlab("Time") +
     ylab("Acceleration (in gravities)")
 
-ukb_accel[1:100,] |>
-  filter(time >= dt & time < dt + minutes(5) ) |>
+ukb_accel[1:100, ] |>
+  filter(time >= dt & time < dt + minutes(5)) |>
   spectral_signature(take_log = TRUE) |>
- # filter(freq <= 10) |>
   accel_plot()
-
-
